a80a89f37b3f28181918e3af6541c309a9f2fc47
DannyRega <daniel.regalado.alcazar@gmail.com>
2026-09-04 02:58:50 -0600

    Actualización en Documentación Swagger Title

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -45,7 +45,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "TaskManager API", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Booking API", Version = "v1" });
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
         Name = "Authorization",
